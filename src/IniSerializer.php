4667f90ab054f5f0f97235e0bff2572210a0176e
Dávid Andor <david-andor@kozpontiagy.hu>
2018-11-04 13:27:03 +0100

    Basic implementation

---
+++ src/IniSerializer.php
@@ -0,0 +1,359 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Sweetchuck\IniSerializer;
+
+class IniSerializer
+{
+    // region commentChars
+    /**
+     * @var array
+     */
+    protected $commentChars = [';', '#'];
+
+    public function getCommentChars(): array
+    {
+        return $this->commentChars;
+    }
+
+    /**
+     * @return $this
+     */
+    public function setCommentChars(array $commentChars)
+    {
+        $this->commentChars = $commentChars;
+
+        return $this;
+    }
+    // endregion
+
+    // region valueNull
+    /**
+     * @var array
+     */
+    protected $valueNull = ['null', 'nil'];
+
+    public function getValueNull(): array
+    {
+        return $this->valueNull;
+    }
+
+    /**
+     * @return $this
+     */
+    public function setValueNull(array $valueNull)
+    {
+        $this->valueNull = $valueNull;
+
+        return $this;
+    }
+    // endregion
+
+    // region valueBoolTrue
+    /**
+     * @var array
+     */
+    protected $valueBoolTrue = ['true', 'on', 'yes'];
+
+    public function getValueBoolTrue(): array
+    {
+        return $this->valueBoolTrue;
+    }
+
+    /**
+     * @return $this
+     */
+    public function setValueBoolTrue(array $valueBoolTrue)
+    {
+        $this->valueBoolTrue = $valueBoolTrue;
+
+        return $this;
+    }
+    // endregion
+
+    // region valueBoolFalse
+    /**
+     * @var array
+     */
+    protected $valueBoolFalse = ['false', 'off', 'no'];
+
+    public function getValueBoolFalse(): array
+    {
+        return $this->valueBoolFalse;
+    }
+
+    /**
+     * @return $this
+     */
+    public function setValueBoolFalse(array $valueBoolFalse)
+    {
+        $this->valueBoolFalse = $valueBoolFalse;
+
+        return $this;
+    }
+    // endregion
+
+    // region quoteStrings
+    /**
+     * @var bool
+     */
+    protected $quoteStrings = false;
+
+    public function getQuoteStrings(): bool
+    {
+        return $this->quoteStrings;
+    }
+
+    /**
+     * @return $this
+     */
+    public function setQuoteStrings(bool $quoteStrings)
+    {
+        $this->quoteStrings = $quoteStrings;
+
+        return $this;
+    }
+    // endregion
+
+    // region spaceAroundEqualSign
+    /**
+     * @var bool
+     */
+    protected $spaceAroundEqualSign = false;
+
+    public function getSpaceAroundEqualSign(): bool
+    {
+        return $this->spaceAroundEqualSign;
+    }
+
+    /**
+     * @return $this
+     */
+    public function setSpaceAroundEqualSign(bool $spaceAroundEqualSign)
+    {
+        $this->spaceAroundEqualSign = $spaceAroundEqualSign;
+
+        return $this;
+    }
+    // endregion
+
+    /**
+     * @var string[]
+     */
+    protected $ini = [];
+
+    /**
+     * @var array
+     */
+    protected $iniCurrent = [];
+
+    /**
+     * @return $this
+     */
+    public function setOptions(array $options)
+    {
+        if (array_key_exists('commentChars', $options)) {
+            $this->setCommentChars($options['commentChars']);
+        }
+
+        if (array_key_exists('valueNull', $options)) {
+            $this->setValueNull($options['valueNull']);
+        }
+
+        if (array_key_exists('valueBoolTrue', $options)) {
+            $this->setValueBoolTrue($options['valueBoolTrue']);
+        }
+
+        if (array_key_exists('valueBoolFalse', $options)) {
+            $this->setValueBoolFalse($options['valueBoolFalse']);
+        }
+
+        if (array_key_exists('quoteStrings', $options)) {
+            $this->setQuoteStrings($options['quoteStrings']);
+        }
+
+        if (array_key_exists('spaceAroundEqualSign', $options)) {
+            $this->setSpaceAroundEqualSign($options['spaceAroundEqualSign']);
+        }
+
+        return $this;
+    }
+
+    public function parse(string $ini): array
+    {
+        $data = [];
+        $dataGroup =& $data;
+
+        $commentChars = $this->getCommentChars();
+        $lines = preg_split('/[\r\n]+/', $ini);
+        // @todo Support for multiline values.
+        foreach ($lines as $line) {
+            $line = trim($line);
+            if (!$line || in_array(mb_substr($line, 0, 1), $commentChars)) {
+                continue;
+            }
+
+            if ($this->isGroupHeader($line)) {
+                $groupName = $this->decodeGroupName(mb_substr($line, 1, -1));
+                if (!array_key_exists($groupName, $data)) {
+                    $data[$groupName] = [];
+                }
+
+                $dataGroup =& $data[$groupName];
+
+                continue;
+            }
+
+            list($key, $value) = preg_split('/=/', $line, 2) + [1 => ''];
+            $dataGroup[trim($key)] = $this->decodeValue(trim($value));
+        }
+
+        return $data;
+    }
+
+    public function emit(array $data): string
+    {
+        $this->ini = [];
+        foreach ($data as $groupName => $keyValuePairs) {
+            if (is_iterable($keyValuePairs)) {
+                $this->emitAddGroup($groupName, $keyValuePairs);
+                continue;
+            }
+
+            $this->emitAddKeyValue($groupName, $keyValuePairs);
+        }
+
+        $this->emitEnsureEmptyLine();
+
+        $string = implode(PHP_EOL, $this->ini);
+        $this->ini = [];
+
+        return $string;
+    }
+
+    protected function emitAddGroup(string $groupName, iterable $keyValuePairs)
+    {
+        $this->emitEnsureEmptyLine();
+        $this->ini[] = sprintf('[%s]', $this->encodeGroupName($groupName));
+
+        foreach ($keyValuePairs as $key => $value) {
+            $this->emitAddKeyValue($key, $value);
+        }
+
+        $this->ini[] = '';
+
+        return $this;
+    }
+
+    protected function emitAddKeyValue(string $key, $value)
+    {
+        $pattern = $this->getSpaceAroundEqualSign() ? '%s = %s' : '%s=%s';
+        $this->ini[] = sprintf($pattern, $key, $this->encodeValue($value));
+
+        return $this;
+    }
+
+    protected function emitEnsureEmptyLine()
+    {
+        if ($this->ini && end($this->ini) !== '') {
+            $this->ini[] = '';
+        }
+
+        return $this;
+    }
+
+    protected function encodeGroupName(string $groupName): string
+    {
+        return strtr($groupName, ['[' => '\\x5b', ']' => '\\x5d']);
+    }
+
+    protected function decodeGroupName(string $groupName): string
+    {
+        return strtr($groupName, ['\\x5b' => '[', '\\x5d' => ']']);
+    }
+
+    protected function encodeValue($value): string
+    {
+        if ($value === null) {
+            return $this->getValueNull()[0];
+        }
+
+        if (is_bool($value)) {
+            return $value ?
+                $this->getValueBoolTrue()[0]
+                : $this->getValueBoolFalse()[0];
+        }
+
+        if (is_numeric($value)) {
+            return (string)$value;
+        }
+
+        if ($this->getQuoteStrings()) {
+            return sprintf('"%s"', $value);
+        }
+
+        $protectedValues = $this->getProtectedValues();
+        if (array_search(mb_strtolower($value), $protectedValues)) {
+            return sprintf('"%s"', $value);
+        }
+
+        return $value;
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function decodeValue(string $input)
+    {
+        // @todo Support for octal and hexadecimal numbers.
+        if (is_numeric($input)) {
+            settype($input, (mb_strpos($input, '.') ? 'float' : 'int'));
+
+            return $input;
+        }
+
+        $inputLower = mb_strtolower($input);
+        if ($input === '' || in_array($inputLower, $this->getValueNull())) {
+            return null;
+        }
+
+        if (in_array($inputLower, $this->getValueBoolTrue())) {
+            return true;
+        }
+
+        if (in_array($inputLower, $this->getValueBoolFalse())) {
+            return false;
+        }
+
+        if (preg_match('/^".*"$/', $input)) {
+            // @todo Strip slashes.
+            return mb_substr($input, 1, -1);
+        }
+
+        return $input;
+    }
+
+    /**
+     * @return string[]
+     */
+    protected function getProtectedValues(): array
+    {
+        $values = [];
+        $raw = array_merge(
+            $this->getValueNull(),
+            $this->getValueBoolTrue(),
+            $this->getValueBoolFalse()
+        );
+        foreach ($raw as $value) {
+            $values[] = mb_strtolower($value);
+        }
+
+        return array_unique($values);
+    }
+
+    protected function isGroupHeader(string $line): bool
+    {
+        return preg_match('/^\[.*\]$/', $line) === 1;
+    }
+}
---
+++ tests/unit/IniSerializerTest.php
@@ -0,0 +1,153 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Sweetchuck\IniSerializer\Test\Unit;
+
+use Sweetchuck\IniSerializer\IniSerializer;
+
+/**
+ * @covers \Sweetchuck\IniSerializer\IniSerializer
+ */
+class IniSerializerTest extends \Codeception\Test\Unit
+{
+    public function casesParse(): array
+    {
+        return [
+            'empty' => [
+                [],
+                '',
+            ],
+            'all-in-one' => [
+                [
+                    'a' => 'b',
+                    'c' => 'd',
+                    'e' => 'f',
+                    '.g' => [
+                        '.h' => -1,
+                        'i' => -0.5,
+                        'j' => 0,
+                        'k' => 0,
+                        'l' => 0.5,
+                        'm' => 1,
+                    ],
+                    'n-[-]-n' => [
+                        'o' => null,
+                        'p' => null,
+                        'q' => true,
+                        'r' => false,
+                        's' => '',
+                    ],
+                ],
+                implode(PHP_EOL, [
+                    'a = b',
+                    'c= d',
+                    'e =f',
+                    '[.g]',
+                    '.h = -1',
+                    'i = -0.5',
+                    'j = 0',
+                    'k = -0',
+                    'l = 0.5',
+                    'm = 1',
+                    '# MyComment 01',
+                    '; MyComment 01',
+                    '[n-\\x5b-\\x5d-n]',
+                    'o =',
+                    'p = null',
+                    'q = true',
+                    'r = false',
+                    's = ""',
+                    '',
+                ]),
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider casesParse
+     */
+    public function testParse(array $expected, string $ini): void
+    {
+        static::assertSame($expected, (new IniSerializer())->parse($ini));
+    }
+
+    public function casesEmit(): array
+    {
+        return [
+            'basic' => [
+                implode(PHP_EOL, [
+                    'a=b',
+                    '',
+                    '[c]',
+                    '',
+                    '[.d]',
+                    '.d:e=null',
+                    'f=true',
+                    'g=false',
+                    'h=-1',
+                    'i=-0.5',
+                    'j=0',
+                    'k=0.5',
+                    'l=1',
+                    'm=n',
+                    '',
+                    '[o-\\x5b-\\x5d-p]',
+                    'r=s',
+                    '',
+                ]),
+                [
+                    'a' => 'b',
+                    'c' => [],
+                    '.d' => [
+                        '.d:e' => null,
+                        'f' => true,
+                        'g' => false,
+                        'h' => -1,
+                        'i' => -0.5,
+                        'j' => 0,
+                        'k' => 0.5,
+                        'l' => 1,
+                        'm' => 'n',
+                    ],
+                    'o-[-]-p' => [
+                        'r' => 's',
+                    ],
+                ],
+            ],
+            'quoteStrings; spaceAroundEqualSign' => [
+                implode(PHP_EOL, [
+                    'a = "b"',
+                    '',
+                ]),
+                [
+                    'a' => 'b',
+                ],
+                [
+                    'quoteStrings' => true,
+                    'spaceAroundEqualSign' => true,
+                ],
+            ],
+            'protectedValues' => [
+                implode(PHP_EOL, [
+                    'a="true"',
+                    '',
+                ]),
+                [
+                    'a' => 'true',
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider casesEmit
+     */
+    public function testEmit(string $expected, array $data, array $options = []): void
+    {
+        $serializer = new IniSerializer();
+        $serializer->setOptions($options);
+
+        static::assertSame($expected, $serializer->emit($data));
+    }
+}
